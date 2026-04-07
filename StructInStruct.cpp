1ae4653619024c9c84213823e737996442e701ab
pichiefox <citrazad@gmail.com>
2026-04-07 15:26:33 +0700

    Struct Detail Alamat dan orang

--- StructInStruct.cpp
+++ StructInStruct.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 using namespace std;
 
+struct DetailAlamat{
+    string desa;
+    string kota;
+};
+Struct orang{
+    string nama; 
+    DetailAlamat alamat;
+    int umur;
+};
