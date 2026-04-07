b93c965f9d3ef3fba548be906cb4da420fb54a11
pichiefox <citrazad@gmail.com>
2026-04-07 15:26:58 +0700

    int main

--- StructInStruct.cpp
+++ StructInStruct.cpp
@@ -10,3 +10,23 @@ Struct orang{
     DetailAlamat alamat;
     int umur;
 };
+
+int main(){
+    //Deklarasi obyek dari struct orang
+    orang mhs;
+    //Isi Objek
+    cout << "Nama Mahasiswa: ";
+    cin >> mhs.nama;
+    cout << "Alamat Desa: ";
+    cin >> mhs.alamat.desa;
+    cout << "Alamat Kota: ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur: ";
+    cin >> mhs.umur;
+    cout << endl;
+    //Menampilkan isi obyek
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+    cout << endl;
+}
