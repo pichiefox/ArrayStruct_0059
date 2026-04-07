5d2e543f18a56246b57fb49082c3dc5b9f55464b
pichiefox <citrazad@gmail.com>
2026-04-07 14:50:39 +0700

    Deklarasi tanpa inisialisasi

--- Array.cpp
+++ Array.cpp
@@ -4,4 +4,7 @@ using namespace std;
 int main(){
     //Deklarasi Array dengan inisialisasi
     int dataku[5] = {10, 20, 30, 40, 50};
+    //Deklarasi tanpa inisialisasi
+    int datamu[5];
+    int x;
 }
