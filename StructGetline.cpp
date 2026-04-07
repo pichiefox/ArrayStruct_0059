370040d45a901d94d40c8c2fabb53d70fa33da47
pichiefox <citrazad@gmail.com>
2026-04-07 15:30:22 +0700

    getline

---
+++ StructGetline.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat{
+    string desa;
+    string kota;
+};
+Struct orang{
+    string nama; 
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main(){
+    //Deklarasi obyek dari struct orang
+    orang mhs;
+    //Isi Objek
+    cout << "Nama Mahasiswa: ";
+    getline(cin, mhs.nama);
+    cout << "Alamat Desa: ";
+    getline(cin, mhs.alamat.desa);
+    cout << "Alamat Kota: ";
+    getline(cin, mhs.alamat.kota);
+    cout << "Umur: ";
+    cin >> mhs.umur;
+    cout << endl;
+    //Menampilkan isi obyek
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+    cout << endl;
+}
